2831e875f19a7e56d4c03206fffb8201e52af2e4
kbej <kbej@itu.dk>
2025-04-02 19:37:57 +0200

    collision

--- main.py
+++ main.py
@@ -25,11 +25,19 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((51,51,51))
 
-    pygame.draw.circle(screen, "red", [state[0], state[1]], robot_radius)
+    # draw robot
+    robot = pygame.draw.circle(screen, "red", [state[0], state[1]], robot_radius)
  
-
+    # draw direction on robot
     pygame.draw.line(screen, (255, 255, 255), [state[0], state[1]], [np.cos(state[2])* robot_radius + state[0], np.sin(state[2])* 40 + state[1]], 5)
     
+    #draw wall
+    #pygame.draw.line(screen, "white", [500, 900], [500, 200], 10)
+
+    wall = pygame.Rect(750, 80, 300, 100)
+    pygame.draw.rect(screen, (255,255,255), wall)
+
+
     # Key events
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -45,14 +53,33 @@ while running:
     # Going straight 
     if keys[pygame.K_f]:
         v_right= v_left
-
+    
+    
+    
     #Update State
     state_change = differential_drive_kinematics(state, dt, v_left , v_right)
-    state = [state[0] + state_change[0], state[1]+ state_change[1], (state[2] + state_change[2]) % 360]
+    newstate = [state[0] + state_change[0], state[1]+ state_change[1], (state[2] + state_change[2]) % 360]
+    
+    collide = wall.colliderect(robot)
+    if collide:
+        if state[0] < wall.x: #and state[1] > wall.y:
+            newstate[0] = wall.x - robot_radius
+            print("left")
 
+        elif state[0] > wall.x + wall.width: #and state[1] > wall.y:
+            newstate[0] = wall.x + wall.width + robot_radius
+            print("right")
+        elif state[1] < wall.y: #and state[0] > wall.x:
+            newstate[1] = wall.y - robot_radius
+            print("top")
+        elif state[1] > wall.y + wall.height: #and state[0] > wall.x:
+            newstate[1] = wall.y + wall.height + robot_radius
+            print("bottom")
+    
+    state = newstate
+    
     # Shows on display
     pygame.display.flip()
-
     dt = clock.tick(60) / 1000
 
 pygame.quit()
