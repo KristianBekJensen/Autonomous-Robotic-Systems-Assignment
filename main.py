38f10d29db55580530c8470c6fa76429e421eeab
kbej <kbej@itu.dk>
2025-04-04 16:03:13 +0200

    collision

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from turtle import width
+from matplotlib.pylab import rand
 import pygame
 import math
 import numpy as np
+from sympy import true
 from kinematics import differential_drive_kinematics
 import sensors as sn
 
@@ -8,9 +11,9 @@ import sensors as sn
 pygame.init()
 
 display_width = 1000
-display_height = 500
+display_height = 1000
 # set up the grid
-num_rows = 10
+num_rows = 20
 num_cols = 20
 row_size = display_height / num_rows
 col_size = display_width / num_cols
@@ -23,13 +26,24 @@ dt = 0
 x = col_size / 2 + col_size
 y = row_size / 2 + row_size
 theta = 0
-
+x = 200
+y = 200
 v_left = 0
 v_right = 0
 r = 40 # robot radius
 
 maze_string = """
 11111111111111111111
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
+10000000000000000001
 10000000000000000001
 10000000000000000001
 10000000000000000001
@@ -47,6 +61,20 @@ walls = []
 value = 100 - r
 senors_values = np.full(12, value)
 
+# make the walls 
+for row in range(num_rows):
+    for col in range(num_cols):
+        if maze_list[row][col] == "1":
+            x_pos = col * col_size
+            y_pos = row * row_size
+            wall_maze = pygame.Rect(x_pos, y_pos, col_size, row_size)
+            walls.append(wall_maze)
+        elif maze_list[row][col] == "2":
+            x_pos = col * col_size
+            y_pos = row * row_size
+            wall_maze = pygame.Rect(x_pos, y_pos, col_size, row_size)
+            pygame.draw.rect(screen, (0,255,0), wall_maze)
+
 def draw_robot_text(x, y, r, theta, angle, text):
     my_font = pygame.font.SysFont('Comic Sans MS', 14)
     
@@ -62,7 +90,7 @@ def draw_robot_text(x, y, r, theta, angle, text):
     text_surface = my_font.render(text, False, (0, 0, 0))
     screen.blit(text_surface, (x2,y2))
 
-    
+
 
     
 
@@ -73,24 +101,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((80,80,80))
-    
-    # draw the walls 
-    for row in range(num_rows):
-        for col in range(num_cols):
-            if maze_list[row][col] == "1":
-                x_pos = col * col_size
-                y_pos = row * row_size
-                wall_maze = pygame.Rect(x_pos, y_pos, col_size, row_size)
-                walls.append(wall_maze)
-                pygame.draw.rect(screen, (255,255,255), wall_maze)
-            elif maze_list[row][col] == "2":
-                x_pos = col * col_size
-                y_pos = row * row_size
-                wall_maze = pygame.Rect(x_pos, y_pos, col_size, row_size)
-                pygame.draw.rect(screen, (0,255,0), wall_maze)
+
+    # draw walls  
+    for wall in walls:
+        color = list(np.random.choice(range(256), size=3))
+        pygame.draw.rect(screen, color, wall)
 
     # calculate new (potential) state
     state = [x, y, theta]
@@ -102,21 +119,95 @@ while running:
     new_robot_rect = pygame.Rect(new_x - r, new_y - r, 2 * r, 2 * r) # rectangle representation of the robot
 
     # check for colliding
-    for wall in walls:
-        
-        if wall.colliderect(new_robot_rect):
-            if new_x < wall.x:
-                new_x = wall.x - r
-            elif new_x > wall.x + wall.width:
-                new_x = wall.x + wall.width + r
-            elif new_y < wall.y:
-                new_y = wall.y - r
-            elif new_y > wall.y + wall.height:
-                new_y = wall.y + wall.height + r
+    def check_x_wall(wall, new_x):
+        if new_x < wall.x:
+            new_x = wall.x - r
+            print("left")
+        elif new_x > wall.x + wall.width:
+            new_x = wall.x + wall.width + r
+            print("right")
+        return new_x
     
+    def check_y_wall(wall, new_y):
+        if new_y < wall.y:
+            new_y = wall.y - r
+            print("above")
+        elif new_y > wall.y + wall.height:
+            new_y = wall.y + wall.height + r
+            print("below")
+        return new_y
+        
+    colliding_walls = []
+
+    for wall in walls:
+        if wall.colliderect(new_robot_rect):
+            colliding_walls.append(wall)
+    
+    if len(colliding_walls) == 1:
+            new_x = check_x_wall(colliding_walls[0], new_x)
+            new_y = check_y_wall(colliding_walls[0], new_y)
+    elif len(colliding_walls) == 2:  
+        if abs(colliding_walls[0].x - colliding_walls[1].x) == col_size:
+            new_y = check_y_wall(colliding_walls[0],new_x)
+        elif abs(colliding_walls[0].y - colliding_walls[1].y) == row_size:
+            new_x = check_x_wall(colliding_walls[0], new_y)
+    elif len(colliding_walls) > 2:  
+        x_coords = np.array([wall.x for wall in colliding_walls])
+
+        # Group rects by unique x values
+        unique_x = np.unique(x_coords)
+        grouped_by_x = [
+            [colliding_walls[i] for i in np.where(x_coords == x)[0]]
+            for x in unique_x
+        ]
+
+        y_coords = np.array([wall.y for wall in colliding_walls])
+
+        # Group rects by unique y values
+        unique_y = np.unique(y_coords)
+        grouped_by_y = [
+            [colliding_walls[i] for i in np.where(y_coords == y)[0]]
+            for y in unique_y
+        ]
+        
+        if len(grouped_by_x) < len(grouped_by_y):
+            grouped_by = grouped_by_x
+        else:
+            grouped_by = grouped_by_y
+
+        grouped_by.sort(key=len,reverse=True)
+
+        if len(grouped_by) == 1:
+            if abs(colliding_walls[0].x - colliding_walls[1].x) == col_size:
+                new_y = check_y_wall(colliding_walls[0],new_x)
+            elif abs(colliding_walls[0].y - colliding_walls[1].y) == row_size:
+                new_x = check_x_wall(colliding_walls[0], new_y)
+        else:
+            for i in range(len(grouped_by)):
+                if grouped_by[0][0].x == grouped_by[0][1].x:
+                    if y < grouped_by[1][0].y: #above
+                        new_y = grouped_by[1][0].y - r
+                    else: # below
+                        new_y = grouped_by[1][0].y + row_size + r
+                    if x < grouped_by[0][0].x: # left
+                        new_x = grouped_by[0][0].x - r
+                    else: #right
+                        new_x = grouped_by[0][0].x + col_size + r
+                
+                elif grouped_by[0][0].y == grouped_by[0][1].y:
+                    if y < grouped_by[0][0].y: #above
+                        new_y = grouped_by[0][0].y - r
+                    else: # below
+                        new_y = grouped_by[0][0].y + row_size + r
+                    if x < grouped_by[1][0].x: # left
+                        new_x = grouped_by[1][0].x - r
+                    else: #right
+                        new_x = grouped_by[1][0].x + col_size + r
+
+
     # updating state variables
     x, y, theta = new_x, new_y, new_theta
-
+    
     # draw the robot with a direction line
     robot = pygame.draw.circle(screen, "red", (x, y), r)
     pygame.draw.line(screen, (255, 255, 255), (x, y), (np.cos(theta) * r + x, np.sin(theta) * r + y), 5)
