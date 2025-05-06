872d122edd85b74634cb54812aa22dc97d66bbf6
kbej <kbej@itu.dk>
2025-05-06 15:16:20 +0200

    increase turn speed

--- navigate.py
+++ navigate.py
@@ -12,9 +12,9 @@ def navigate(keys, v_left, v_right):
         v_left = max(0, v_left - 0.2)
         v_right = max(0, v_right - 0.2)
     elif keys[pygame.K_d]:
-        v_right *= 0.99
+        v_right *= 0.98
     elif keys[pygame.K_a]:
-        v_left *= 0.99
+        v_left *= 0.98
     elif keys[pygame.K_e]:
         if v_left == 0 and v_right == 0:
             v_left = 0.3
