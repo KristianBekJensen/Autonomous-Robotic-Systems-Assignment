9994be8c5b47a9c577be2dc41c004e0332e7596c
kbej <kbej@itu.dk>
2025-05-14 16:22:23 +0200

    update replay

--- replay_best_nn.py
+++ replay_best_nn.py
@@ -1,6 +1,6 @@
 import pickle
 from maze_solver_nn import MazeSolver
-from config_nn import num_sensors, input_size, hidden_size, output_size
+from config_nn_target_controller import num_sensors, input_size, hidden_size, output_size, genome_length, pop_size, generations, max_steps, random_map, close_controller, start_pop_filename, controller, fitness_func
 
 
 # Load the final population
@@ -12,16 +12,22 @@ with open("populations/final_nn_pop.pkl", "rb") as f:
 best = min(final_pop, key=lambda ind: ind.fitness)
 print("Best fitness:", best.fitness)
 
+mapSeed = 44
 
 # Visualize the best individual of the population
-visual_solver = MazeSolver(
-    maximize=False,
-    visualization=True,
-    num_sensors=num_sensors,
-    input_size=input_size,
-    hidden_size=hidden_size,
-    output_size=output_size
-)
+problem = MazeSolver(
+            maximize=False,
+            visualization=True,
+            num_sensors=num_sensors,
+            input_size=input_size,
+            hidden_size=hidden_size,
+            output_size=output_size,
+            max_steps=max_steps,
+            close_controller=close_controller,
+            controller_type=controller,
+            fitness_func=fitness_func,
+            random = mapSeed
+        )
 
 # run the best individual
-visual_solver.evaluate(best.phenome)
+problem.evaluate(best.phenome)
